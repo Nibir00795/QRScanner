8dfdb6f33ee36bf8f3041b592029c75f1ba8960f
Nibir <jonayedhossain1995@gmail.com>
2020-07-12 20:52:57 +0600

    Initial Commit

---
+++ QRScanner/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  QRScanner
+//
+//  Created by Mobioapp on 12/7/20.
+//  Copyright © 2020 Nibir. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
